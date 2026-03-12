256c4046dcee15b36e4c7f70326a253c1f537100
Matias <hola111@gmail.com>
2026-03-12 20:01:43 -0300

    fix: rename PrismaLibSQL to PrismaLibSql for vercel build

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from '@prisma/client'
-import { PrismaLibSQL } from '@prisma/adapter-libsql'
+import { PrismaLibSql } from '@prisma/adapter-libsql'
 import { createClient } from '@libsql/client'
 
 const prismaClientSingleton = () => {
@@ -12,7 +12,7 @@ const prismaClientSingleton = () => {
       url: connectionString,
       authToken,
     })
-    const adapter = new PrismaLibSQL(libsql)
+    const adapter = new PrismaLibSql(libsql)
     return new PrismaClient({ adapter })
   }
 
